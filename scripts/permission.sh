cddb2edaa79c3ee1a582d0069914d68f71ab528b
Ankit Anand <64396916+ankitanand12@users.noreply.github.com>
2021-07-11 23:58:55 +0530

    Update permission.sh

--- scripts/permission.sh
+++ scripts/permission.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-chmod -R 755 /var/www/html/ankit.squareops.xyz/public_html/wp-content
-chown -R www-data:www-data /var/www/html/ankit.squareops.xyz/public_html/wp-content
+chmod -R 755 /var/www/html/ankit.squareops.xyz/public_html/wp-content/uploads/
+chown -R www-data:www-data /var/www/html/ankit.squareops.xyz/public_html/wp-content/uploads
