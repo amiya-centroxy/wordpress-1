0b12f597b9ee67ac701bddc272f541823aa32b18
Ankit Anand <64396916+ankitanand12@users.noreply.github.com>
2021-07-12 11:52:50 +0530

    Update permission.sh

--- scripts/permission.sh
+++ scripts/permission.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-chmod -R 755 /var/www/html/ankitword.squareops.xyz/public_html/wp-content/uploads/
-chown -R www-data:www-data /var/www/html/ankitword.squareops.xyz/public_html/wp-content/uploads/
-chmod -R 755 /var/www/html/ankitword.squareops.xyz/public_html/wp-content/
-chown -R www-data:www-data /var/www/html/ankitword.squareops.xyz/public_html/wp-content/
+chmod -R 755 /var/www/html/wp-content/uploads/
+chown -R www-data:www-data /var/www/html/wp-content/uploads/
