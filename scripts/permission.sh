8c67d08889bfe6ed0c63ec7c410b8726efe311d5
Amiyakumar246 <66254829+Amiyakumar246@users.noreply.github.com>
2021-07-27 20:20:03 +0530

    Update permission.sh

--- scripts/permission.sh
+++ scripts/permission.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod -R 755 /var/www/html/amiya.squareops.co.in/
-chown -R www-data:www-data /var/www/html/amiya.squareops.co.in/
-chmod -R 755 /var/www/html/amiya.squareops.co.in/
-chown -R www-data:www-data /var/www/html/amiya.squareops.co.in/
+chmod -R 755 /var/www/html/
+chown -R www-data:www-data /var/www/html/
+chmod -R 755 /var/www/html/
+chown -R www-data:www-data /var/www/html/
